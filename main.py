5163b7d9d676f9f0f6f0e7a4dd54e0cda66acf6f
username <emanhmc@gmail.com>
2025-01-16 20:38:10 +0700

    Change completion check method

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ class Main:
             if self.review_only:
                 break
             try:
-                completed = quiz_div.find_element(By.XPATH, "//title[text()='Completed']")
+                completed = quiz_div.find_element(By.CSS_SELECTOR, "svg#cds-react-aria-80")
                 if completed:
                     print(f"Skipping {quiz_div.get_attribute('innerText')}")
                     continue
